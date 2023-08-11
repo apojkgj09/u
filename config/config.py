ce23ec3de16a42500a4edd3395b3b5499218fd83
Quantomize <131014805+mrHypertrix@users.noreply.github.com>
2023-08-11 17:32:27 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -29,7 +29,7 @@ SONG_DOWNLOAD_DURATION = int(
 )  # Remember to give value in Minutes
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001687657146")
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001687657146"))
 
 # A name for your Music bot.
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME","ArchMusic")
